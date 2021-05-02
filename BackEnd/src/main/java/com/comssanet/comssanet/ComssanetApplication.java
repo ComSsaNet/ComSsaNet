ee5145cc172388d0814764fa29ccdfd601f4eb5d
yunhwan <pyh8618@gmail.com>
2021-05-02 14:37:11 +0900

    project setting & skeleton code 작성

---
+++ BackEnd/src/main/java/com/comssanet/comssanet/ComssanetApplication.java
@@ -0,0 +1,13 @@
+package com.comssanet.comssanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComssanetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComssanetApplication.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/com/comssanet/comssanet/ComssanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comssanet.comssanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComssanetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
