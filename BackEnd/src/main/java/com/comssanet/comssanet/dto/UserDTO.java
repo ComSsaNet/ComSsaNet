db827efccc3948eca712910e1a435db676e646f0
yunhwan <pyh8618@gmail.com>
2021-05-02 14:34:16 +0900

    project setting & skeleton code 작성

---
+++ BackEnd/src/main/java/com/comssanet/comssanet/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.comssanet.comssanet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long user_id;
+}
---
+++ BackEnd/src/main/java/com/comssanet/comssanet/service/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.comssanet.comssanet.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+}
