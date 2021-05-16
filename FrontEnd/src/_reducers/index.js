a6b21016d841ea675eabc5254e7691cb1a10f5c0
OH-JOO-YEONG <brb1111@naver.com>
2021-05-16 20:17:27 +0900

    feat: 폴더구조 구축 및 Redux, Redux-saga 환경 구축
    폴더 구조
    routes - 각 페이지 (페이지별 폴더 생성)
     Router.js - 각 페이지 라우터 연결
    
    components - 컴포넌트
    
    _reducers - 리듀서 폴더
    
    _sagas - 사가 폴더
    
    Redux, Redux-saga 환경 구축
    - index.js에 store 설정 및 각각 폴더 index.js에서 reducer, saga 설정

---
+++ FrontEnd/src/_reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import user from "./user";
+
+// 루트 리듀서 - 전체 리듀서를 모아주는 역할
+const rootReducer = combineReducers({
+    user,
+});
+
+export default rootReducer;
