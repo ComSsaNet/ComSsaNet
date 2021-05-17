c8722ccc28af2d961ab8c89175a95a60e4716d99
OH-JOO-YEONG <brb1111@naver.com>
2021-05-18 06:55:37 +0900

    chore: 폴더 git 스테이징 버그 이슈
    git 스테이징 버그 예전에 FrontEnd 폴더로 만들었던거
    폴더이름을 frontend로 바꾸면서 CRA로 환경 구축했는데 git 스테이징이 FrontEnd로 되면서
    알수 없는 버그가 발생함.
    삭제 조치하기로 했음.

--- FrontEnd/src/_reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import user from "./user";
-
-// 루트 리듀서 - 전체 리듀서를 모아주는 역할
-const rootReducer = combineReducers({
-    user,
-});
-
-export default rootReducer;
